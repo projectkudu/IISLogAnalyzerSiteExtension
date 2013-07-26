9def0738ca5afc641bd5b933b163d8beb2e0e800
-v <t-hawkf@microsoft.com>
2013-07-26 10:43:17 -0700

    Forgot to include the bookmark classes in last commit.

---
+++ WebAnalytics/Models/LogBookmarks/LogFileBookmark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAnalytics.Models.BookmarkAPI;
+
+namespace WebAnalytics.Models.LogBookmarks
+{
+    public class LogFileBookmark:Bookmark
+    {
+        public string FilePath { get; set; }
+        public string StartDateTime { get; set; }
+        public string EndDateTime { get; set; }
+        public string Location { get; set; }
+        public object Mark
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
