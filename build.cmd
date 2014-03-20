e9754a07e4f3dd65107f4c5e676f0edac503a029
davidebbo <david.ebbo@microsoft.com>
2014-03-19 17:56:19 -0700

    nuget restore

--- build.cmd
+++ build.cmd
@@ -3,4 +3,6 @@ set config=%1
 if "%config%" == "" (
    set config=Release
 )
+
+nuget restore
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /m
