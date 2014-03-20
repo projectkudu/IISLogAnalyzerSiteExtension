240865c080add72b81c702494746cb36ed8016b2
davidebbo <david.ebbo@microsoft.com>
2014-03-19 18:13:38 -0700

    Build nuget package + cleanup

--- build.cmd
+++ build.cmd
@@ -6,3 +6,5 @@ if "%config%" == "" (
 
 nuget restore
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.proj /p:Configuration="%config%";ExcludeXmlAssemblyFiles=false /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false /m
+
+nuget pack -OutputDirectory artifacts\Release
